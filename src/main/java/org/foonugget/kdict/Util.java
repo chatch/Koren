60cfbc3edec6f52e50ad4f282868f95a8530041d
Chris Hatch <devcebfaff@proton.me>
2026-05-01 12:14:55 +1000

    remove old files

--- src/main/java/org/foonugget/kdict/Util.java
+++
@@ -1,9 +0,0 @@
-package org.foonugget.kdict;
-
-public class Util {
-
-    public static String escapeDatabaseStringLiteral(String string) {
-        return string.replaceAll("'", "''");
-    }
-
-}
